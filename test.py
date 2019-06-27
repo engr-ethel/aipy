0f0b57882578fd44fccac0ac712b8b400c90a9b7
engr-ethel <52150122+engr-ethel@users.noreply.github.com>
2019-06-27 09:18:24 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+
+print (" just to test")
+print ("hope will work")
+import matplotlib.pyplot as plt #for showing the image
+import matplotlib.image as mpimg #for reading the image
+image = mpimg.imread('image01.jpeg') #loading/reading the image
+plt.imshow(image) #displaying the image
